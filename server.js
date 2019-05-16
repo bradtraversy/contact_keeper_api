17054589ecf1ea566b8933987c933af3de89acb0
Brad Traversy <traversymedia@gmail.com>
2019-05-16 09:54:24 -0400

    User model and register to get token

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
