9865faa9652e1b3aab998c4bb8f2694c4da6ecac
Brad Traversy <traversymedia@gmail.com>
2019-05-16 09:50:49 -0400

    Initial server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
